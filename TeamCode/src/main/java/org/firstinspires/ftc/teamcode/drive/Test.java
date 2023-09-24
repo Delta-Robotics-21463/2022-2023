2432e317656da3b9d8cdb497b77926336724096f
Vyom Shukla <rajanjshu76@gmail.com>
2023-09-24 10:27:26 -0700

    From vyomshukla

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/Test.java
@@ -0,0 +1,42 @@
+package org.firstinspires.ftc.teamcode.drive;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp
+public class Test extends OpMode {
+    DcMotor LeftForward = null;
+    DcMotor RightForward = null;
+    DcMotor LeftBackward = null;
+    DcMotor RightBackward = null;
+    public void init(){
+        LeftForward = hardwareMap.get(DcMotor.class, "LeftBackward");
+        RightForward = hardwareMap.get(DcMotor.class, "RightForward");
+        LeftBackward = hardwareMap.get(DcMotor.class, "LeftBackward");
+        RightBackward = hardwareMap.get(DcMotor.class, "RightBackward");
+
+        LeftForward.setDirection(DcMotorSimple.Direction.REVERSE);
+        LeftBackward.setDirection(DcMotorSimple.Direction.REVERSE);
+    }
+
+    public void loop(){
+        if(gamepad1.left_stick_y != 0.0){
+            LeftForward.setPower(-gamepad1.left_stick_y);
+            LeftBackward.setPower(-gamepad1.left_stick_x);
+
+        }else{
+            LeftForward.setPower(0.0);
+            LeftBackward.setPower(0.0);
+        }
+
+        if(gamepad1.right_stick_y != 0.0){
+            RightForward.setPower(-gamepad1.right_stick_y);
+            RightBackward.setPower(-gamepad1.right_stick_y);
+        }else{
+            RightForward.setPower(0.0);
+            RightBackward.setPower(0.0);
+        }
+    }
+}
